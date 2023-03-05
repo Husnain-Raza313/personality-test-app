00c31ec5a2ac697cb41cbc887fe13b5f8c3fb348
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-03-05 15:59:09 +0500

    Application: Enabled Bootstrap and added home view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  # root to: "home#index"
+  # # root to: new_admin_session_path
+
+  devise_scope :admin do
+    authenticated :admin do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
