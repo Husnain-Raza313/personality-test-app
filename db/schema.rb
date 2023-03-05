1f28dd2a902cacc22c76a8f67fc1453c0f8de4f7
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-03-05 13:13:36 +0500

    Application: Added Option Model and Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_05_075023) do
+ActiveRecord::Schema.define(version: 2023_03_05_075845) do
+
+  create_table "options", force: :cascade do |t|
+    t.text "content", default: "", null: false
+    t.integer "option_type", default: 0, null: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content", "question_id"], name: "index_options_on_content_and_question_id", unique: true
+    t.index ["question_id"], name: "index_options_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "content", default: "", null: false
@@ -18,4 +28,5 @@ ActiveRecord::Schema.define(version: 2023_03_05_075023) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "options", "questions"
 end
