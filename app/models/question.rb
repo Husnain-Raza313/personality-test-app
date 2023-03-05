59f7b135b7315c3029e61f6dbe0da7a8a0d324a4
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-03-06 03:08:42 +0500

    Application: removed unnecessary code and files, resolved spacing issues and applied rubocop

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Question < ApplicationRecord
   has_many :options, dependent: :destroy
   accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
