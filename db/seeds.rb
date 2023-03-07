021f75cf3d1bf07ee56464e5eca2d9ea4498c388
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-03-07 21:20:41 +0500

    Readme and Seeds: Added some necessary changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.destroy_all
+Question.destroy_all
 
 Admin.create(email: "admin@admin.com", password: "test@123")
 
